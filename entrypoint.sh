4e5ded822d391f5339739ae78af7e05b3eee094b
Hiro Asari <asari.ruby@gmail.com>
2019-01-09 15:25:34 -0500

    Fix curl command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [ -n ${TRAVIS_TOKEN} ]; then
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -H "Travis-API-Version: 3" \
-    -H "Authorization: token ${TRAVIS_TOKEN}"
+    -H "Authorization: token ${TRAVIS_TOKEN}" \
     -d "{request: {}}" \
     https://api.travis-ci.org/BanzaiMan%2Ftravis_production_test/requests
 else
