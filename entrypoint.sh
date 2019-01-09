7b20761ca383f829262feddc0eae46c4c703b7e1
Hiro Asari <asari.ruby@gmail.com>
2019-01-09 15:55:20 -0500

    Make many parts configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+## env vars
+# SLUG - repo name, in the form "OWNER/REPO"
+# SITE - org or com, defaults to com
+
+OWNER=${SLUG%%/*}
+REPO=${SLUG##*/}
+SITE=${SITE:-com}
+
+if [ x${SITE} != "xorg" -a x${SITE} != "xcom" ]; then
+  echo "Invalid \$SITE value: $SITE; use 'org' or 'com'"
+  exit 1
+fi
+
 if [ -n ${TRAVIS_TOKEN} ]; then
   curl -sSf -X POST \
     -H "Content-Type: application/json" \
@@ -9,7 +22,7 @@ if [ -n ${TRAVIS_TOKEN} ]; then
     -H "Travis-API-Version: 3" \
     -H "Authorization: token ${TRAVIS_TOKEN}" \
     -d "{\"request\": {}}" \
-    https://api.travis-ci.org/repo/BanzaiMan%2Ftravis_production_test/requests
+    https://api.travis-ci.${SITE}/repo/${OWNER}%2F${REPO}/requests
 else
   echo "Please set \$TRAVIS_TOKEN"
 fi
